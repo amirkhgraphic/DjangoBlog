be3e143ba39edd4417d49d0aeff5cc3ce853a582
Amirhossein Khalili <amirhosseinkhalili901@gmail.com>
2024-01-31 19:06:32 +0330

    add posts models and admin pannel

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,3 +1,49 @@
+from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
+from apps.posts.models import Category, Post, Comment
 
-# Register your models here.
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    readonly_fields = ['created_at', 'user', 'post']
+    fields = ['user', 'body', 'created_at']
+
+
+class PostInline(admin.StackedInline):
+    model = Post
+    readonly_fields = ['author', 'created_at', 'last_modified']
+    fields = ['title', 'body', 'author', 'created_at', 'last_modified']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
+    fields = ['name']
+    inlines = [PostInline]
+    search_fields = ['name']
+    ordering = ['name']
+    add_fieldsets = (
+        (_('Required Fields'), {'classes': ['wide'], 'fields': ['name']}),
+    )
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'created_at', 'last_modified']
+    list_filter = ['category', 'author']
+    fields = ['title', 'body', 'author', 'category', 'created_at', 'last_modified']
+    inlines = [CommentInline]
+    readonly_fields = ['author', 'created_at', 'last_modified']
+    search_fields = ['title', 'body']
+    ordering = ['-created_at', '-last_modified']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'post', 'created_at']
+    list_filter = ['user', 'post']
+    fields = ['user', 'post', 'body', 'created_at']
+    readonly_fields = ['created_at']
+    search_fields = ['body']
+    ordering = ['-created_at']
--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from apps.users.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=127, unique=True)
+
+    class Meta:
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField(blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='posts', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} on post "{self.post}"'
